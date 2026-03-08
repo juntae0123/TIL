fe6e10d9a849c0af3f3eaa79a0cfd3b83b1f9205
Kim Juntae <juntai0312@gmail.com>
2026-03-09 08:29:10 +0900

    renewal

---
+++ Web/python_ws_4_5/ws_4_5.py
@@ -0,0 +1,356 @@
+user_data = [
+    {
+        'blood_group': 'AB',
+        'company': 'Stone Inc',
+        'mail': 'ian17@yahoo.com',
+        'name': 'Kathryn Jenkins',
+        'website': [
+            'https://www.boyd-herrera.com/',
+            'https://watson.com/',
+            'http://www.mitchell.com/',
+            'http://irwin-cline.biz/',
+        ],
+    },
+    {
+        'blood_group': 'AB+',
+        'company': 'Fleming Ltd',
+        'mail': 'patricianelson@yahoo.com',
+        'name': 'Angel Williamson',
+        'website': [
+            'https://wilson-johnson.com/',
+            'https://santiago-hammond.com/',
+            'https://morales.com/',
+            'https://fry-fleming.com/',
+        ],
+    },
+    {
+        'blood_group': 'A+',
+        'company': 'Scott PLC',
+        'mail': 'lisajones@gmail.com',
+        'name': 'Stephanie Herman MD',
+        'website': [
+            'https://www.boyer-stevens.org/',
+            'http://www.johnson.com/',
+        ],
+    },
+    {
+        'blood_group': 'AB-',
+        'company': 'Warren-Stewart',
+        'mail': 'allisonjennifer@gmail.com',
+        'name': 'Jon Martinez',
+        'website': ['https://www.berg.com/'],
+    },
+    {
+        'blood_group': 'AB+',
+        'company': 'Fisher Inc',
+        'mail': 'mross@yahoo.com',
+        'name': 'Justin Brown',
+        'website': [
+            'https://www.gray.com/',
+            'https://jones.com/',
+            'http://williams.biz/',
+            'https://hammond.net/',
+        ],
+    },
+    {
+        'blood_group': 'B-',
+        'company': 'Pearson Group',
+        'mail': 'gravesbarbara@hotmail.com',
+        'name': 'Bobby Patterson',
+        'website': ['https://www.cunningham.biz/', 'https://johnson.com/'],
+    },
+    {
+        'blood_group': 'AB-',
+        'company': 'White, Andrade and Howard',
+        'mail': 'mcole@gmail.com',
+        'name': 'Michelle Strickland',
+        'website': ['http://www.rose-gomez.com/', 'https://reilly.com/'],
+    },
+    {
+        'blood_group': 'AB-',
+        'company': 'Brown-Young',
+        'mail': 'tmorales@hotmail.com',
+        'name': 'Stephanie Moore',
+        'website': ['https://schmidt.com/'],
+    },
+    {
+        'blood_group': 'AB+',
+        'company': 'Brooks PLC',
+        'mail': 'wellsmatthew@hotmail.com',
+        'name': 'Dr. David Johnson',
+        'website': [
+            'http://ford-dean.com/',
+            'http://www.petersen.com/',
+            'https://thompson-cooley.info/',
+            'http://ryan-gay.com/',
+        ],
+    },
+    {
+        'blood_group': 'A-',
+        'company': 'Stewart Group',
+        'mail': 'sean37@hotmail.com',
+        'name': 'Veronica Webb',
+        'website': ['http://www.holmes.info/', 'http://www.morris.biz/'],
+    },
+    {
+        'blood_group': 'AB+',
+        'company': 'Cabrera, Perry and Harris',
+        'mail': 'bgonzales@yahoo.com',
+        'name': 'Lisa Wilcox',
+        'website': ['https://www.small.com/', 'http://martin-petersen.com/'],
+    },
+    {
+        'blood_group': 'B+',
+        'company': 'Thomas, Lozano and Lopez',
+        'mail': 'bperry@yahoo.com',
+        'name': 'Brian Simmons',
+        'website': [
+            'http://reid.com/',
+            'http://www.roman-neal.biz/',
+            'https://www.hoover.org/',
+            'https://www.lynn.com/',
+        ],
+    },
+    {
+        'blood_group': 'O+',
+        'company': 'Baker-Leach',
+        'mail': 'johnlucas@yahoo.com',
+        'name': 'Carlos Robinson',
+        'website': ['https://martin.com/', 'http://montgomery-cline.com/'],
+    },
+    {
+        'blood_group': 'AB-',
+        'company': 'Higgins, Higgins and Garcia',
+        'mail': 'chris66@gmail.com',
+        'name': 'Gabriel Collins',
+        'website': ['https://www.cole-pugh.com/'],
+    },
+    {
+        'blood_group': 'AB-',
+        'company': 'Tanner, Wheeler and Weaver',
+        'mail': 'leonardtammy@gmail.com',
+        'name': 'Christopher Cook',
+        'website': [
+            'https://www.myers-reynolds.com/',
+            'https://dunlap-rogers.com/',
+            'https://luna.net/',
+            'http://smith-miller.com/',
+        ],
+    },
+    {
+        'blood_group': 'A-',
+        'company': 'Schaefer-Hunter',
+        'mail': 'nsummers@gmail.com',
+        'name': 'Daniel Monroe',
+        'website': [
+            'https://cook.net/',
+            'http://carpenter.com/',
+            'http://morris-terrell.com/',
+        ],
+    },
+    {
+        'blood_group': 'B+',
+        'company': 'Stephens Group',
+        'mail': 'rolson@gmail.com',
+        'name': 'Molly Parks',
+        'website': [
+            'https://wright-vincent.biz/',
+            'http://www.cruz.com/',
+            'http://olson.org/',
+            'http://gomez.com/',
+        ],
+    },
+    {
+        'blood_group': 'O-',
+        'company': 'Fitzgerald, Costa and Hobbs',
+        'mail': 'jennifervang@hotmail.com',
+        'name': 'Jill Patterson',
+        'website': [
+            'https://www.brewer.com/',
+            'https://malone-murray.info/',
+            'http://evans.com/',
+            'https://ortiz.com/',
+        ],
+    },
+    {
+        'blood_group': 'A-',
+        'company': 'Frazier Ltd',
+        'mail': 'vsolis@hotmail.com',
+        'name': 'Marie May',
+        'website': [
+            'http://pratt.info/',
+            'http://www.ortega.com/',
+            'http://www.smith.net/',
+            'https://nichols.biz/',
+        ],
+    },
+    {
+        'blood_group': 'O+',
+        'company': 'Rodriguez and Sons',
+        'mail': 'michael09@yahoo.com',
+        'name': 'Julia Gonzalez',
+        'website': [
+            'https://www.cantrell.com/',
+            'https://www.smith.net/',
+            'http://delgado.com/',
+            'http://stevens.com/',
+        ],
+    },
+    {
+        'blood_group': 'AB-',
+        'company': 'Brown-Arnold',
+        'mail': 'christopher79@hotmail.com',
+        'name': 'David Garza',
+        'website': ['https://price.net/'],
+    },
+    {
+        'blood_group': 'A+',
+        'company': 'Butler-Hernandez',
+        'mail': 'angiechoi@yahoo.com',
+        'name': 'Leslie Kemp',
+        'website': ['http://www.martin-thompson.org/', 'http://martin.org/'],
+    },
+    {
+        'blood_group': 'A-',
+        'company': 'Schneider-Hensley',
+        'mail': 'cesarsantos@hotmail.com',
+        'name': 'Brandon Peterson',
+        'website': [
+            'https://www.owens-gay.com/',
+            'https://www.santiago.org/',
+            'https://www.singleton.com/',
+        ],
+    },
+    {
+        'blood_group': 'O-',
+        'company': 'Hunter, Alvarado and Stewart',
+        'mail': 'thomas16@gmail.com',
+        'name': 'Matthew Stanley',
+        'website': ['https://nelson.com/'],
+    },
+    {
+        'blood_group': 'A+',
+        'company': 'Elliott, Mullins and Michael',
+        'mail': 'smithedward@hotmail.com',
+        'name': 'Robert Brown',
+        'website': [
+            'http://montgomery-rogers.biz/',
+            'http://www.williams-nixon.com/',
+        ],
+    },
+    {
+        'blood_group': 'AB+',
+        'company': 'Velasquez-Garcia',
+        'mail': 'samanthawilson@yahoo.com',
+        'name': 'Stephanie Cohen',
+        'website': ['http://jackson-harris.com/'],
+    },
+    {
+        'blood_group': 'A+',
+        'company': 'Mccoy-Hopkins',
+        'mail': 'lli@yahoo.com',
+        'name': 'Michael Clark',
+        'website': [
+            'https://www.harding.info/',
+            'https://www.jones.biz/',
+            'http://knight-adkins.org/',
+            'http://www.alvarado-mendoza.org/',
+        ],
+    },
+    {
+        'blood_group': 'O+',
+        'company': 'Kerr Ltd',
+        'mail': 'georgebrittany@yahoo.com',
+        'name': 'Brandon White',
+        'website': [
+            'https://flowers-parker.info/',
+            'http://oliver-rice.info/',
+        ],
+    },
+    {
+        'blood_group': 'AB-',
+        'company': 'Villarreal, Wood and Smith',
+        'mail': 'denise73@yahoo.com',
+        'name': 'Kevin Blevins',
+        'website': [
+            'http://www.ramirez.info/',
+            'https://mckay.net/',
+            'http://duran.com/',
+        ],
+    },
+    {
+        'blood_group': 'O+',
+        'company': 'Jenkins-Garcia',
+        'mail': 'kwoodward@hotmail.com',
+        'name': 'Michelle Dixon',
+        'website': [
+            'http://www.taylor.com/',
+            'https://bates-trujillo.org/',
+            'https://www.thomas-boyer.org/',
+        ],
+    },
+]
+
+blood_types = ['A-', 'A+', 'B-', 'B+', 'O-', 'O+', 'AB-', 'AB+']
+black_list = [
+    'Jenkins-Garcia',
+    'Stephens Group',
+    'White, Andrade and Howard',
+    'Warren-Stewart',
+]
+
+
+
+def create_user(user_data):
+    wrong_user_count = 0                      # ← 네 코드: errors_list = []
+    user_list = []                            # ← 네 코드: errors = []; user_list = []
+
+    for user in user_data:
+        status, wrong_fields = is_validation(user)  
+        # ← 네 코드: is_validation(user, errors_list)
+
+        if status == 'blocked':
+            wrong_user_count += 1              # ← 네 코드: len(errors_list)로 유저 수 판단
+            continue                           # ← 네 코드: blocked도 errors에 넣고 나중에 빼려 함
+
+        if status is False:
+            wrong_user_count += 1              # ← 네 코드: 잘못된 유저 수를 따로 세지 않음
+            for field in wrong_fields:          # ← 네 코드: wrong_fields 개념 없음
+                user[field] = None              # ← 네 코드: user_data - errors 로 제거하려 함
+
+        user_list.append(user)                 # ← 네 코드: 끝에서 리스트 빼기 연산 시도
+
+    print(f"잘못된 데이터로 구성된 유저의 수는 {wrong_user_count}입니다.")
+    # ← 네 코드: print(len(errors_list))
+
+    for u in user_list:
+        print(u)                               # ← 네 코드: errors_list만 출력
+
+    return user_list                           # ← 네 코드: return 없음
+
+
+def is_validation(user):
+    wrong_fields = []                          # ← 네 코드: errors_list를 외부에서 받아 누적
+
+    if user['company'] in black_list:
+        return 'blocked', ['company']          # ← 네 코드: elif + append만 하고 return 없음
+
+    if user['blood_group'] not in blood_types:
+        wrong_fields.append('blood_group')     # ← 네 코드: append((user['name'], 'blood_group'))
+
+    if '@' not in user['mail']:
+        wrong_fields.append('mail')            # ← 네 코드: elif라서 위에서 걸리면 실행 안 됨
+
+    if len(user['name']) < 2 or len(user['name']) > 30:
+        wrong_fields.append('name')            # ← 네 코드: append((user['name']), 'name') ← 문법 에러
+
+    if len(user['website']) < 1:
+        wrong_fields.append('website')         # ← 네 코드: append((user['name']), 'website')
+
+    if len(wrong_fields) == 0:
+        return True, []                        # ← 네 코드: return True만
+    else:
+        return False, wrong_fields             # ← 네 코드: return False만 (에러 1개만 기록됨)
+
+
+create_user(user_data)
