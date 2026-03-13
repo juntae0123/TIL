3d280c0021b76d654d7ac8372324c8c21b3ef0fc
Kim Juntae <juntai0312@gmail.com>
2026-03-13 17:04:16 +0900

    0313

---
+++ Livecode/solvement/practice.py
@@ -0,0 +1,26 @@
+T = int(input())
+
+for tc in range(1, 1 + T):
+    N, W, H = map(int, input().split())
+    arr =[]
+    for _ in range(H):
+        arr.append(list(map(int, input().split())))
+
+#----------- 인풋값 받기----------------------------
+
+# bfs 함수(폭발 확인 및 제거) + 중력 함수 + 돌 떨어트리는 함수 
+
+def bfs(cnt, arr):
+    pass
+
+def drop(W):
+    pass
+
+def gravity():
+    pass
+
+def boom():
+
+
+    
+
