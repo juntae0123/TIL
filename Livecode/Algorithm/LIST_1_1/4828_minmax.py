14533429c8798de86b840fe076d8150dbd530c67
juntai0312 <juntai0312@gmail.com>
2026-03-21 15:50:40 +0900

    save

--- Livecode/Algorithm/LIST_1_1/4828_minmax.py
+++
@@ -1,7 +0,0 @@
-T = int(input())
-for test_case in range(1, T+1):
-    jung = int(input())
-    yang = list(map(int, input().split()))
-    mima = sorted(yang)
-    result = abs(mima[jung -1] - mima[0])
-    print(f'#{test_case} {result}')
