460f0dd6c274777aeef6eb4eed18dcd20064c417
juntai0312 <juntai0312@gmail.com>
2026-03-21 15:50:40 +0900

    save

--- Livecode/Algorithm/LIST_2_1/jungryul.py
+++
@@ -1,8 +0,0 @@
-T = int(input())
-for test_case in range(1, T + 1):
-    length = int(input())
-    sut = sorted(list(map(int, input().split())))
-    print(f'#{test_case}', *sut)
-
-
-
--- Livecode/Study/과목평가.py
+++
@@ -1,5 +0,0 @@
-a = int('1011',2)
-print(a)
-b=bin(13)[2:]
-print(hex(28)[2:].upper())
-print(b)
